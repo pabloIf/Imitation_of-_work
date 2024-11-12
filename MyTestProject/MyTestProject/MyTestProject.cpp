50c302816a0d23c9c4f5b69b145f82300a1df8a9
pablo <aknelsaltikov2006@gmail.com>
2024-11-12 13:14:02 +0200

    Add a main.cpp(MyTestProject) file

---
+++ MyTestProject/MyTestProject/MyTestProject.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Canculator {
+public:
+	static int sum(int a, int b) {
+		return a + b;
+	}
+	static int sum(int a, int b, int c) {
+		return a + b + c;
+	}
+	static int subtract(int a, int b) {
+		return a - b;
+	}
+	static int multiply(int a, int b) {
+		return a * b;
+	}
+	static int divive(int a, int b) {
+		if (b == 0) {
+			cout << "Error: Division by zero!" << endl;
+			return 0;
+		}
+		return a / b;
+	}
+};
+
+int main() {
+	int a = 5, b = 3;
+
+	cout << "Sum: " << Canculator::sum(a, b) << endl;
+	cout << "Subtract: " << Canculator::subtract(a, b) << endl;
+	cout << "Multiply: " << Canculator::multiply(a, b) << endl;
+	cout << "Divive: " << Canculator::divive(a, b) << endl;
+}
